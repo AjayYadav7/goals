a2f1551d71ac9bd1e41e743730450dc114081936
Ajayyadav18 <ajay.yadav@aurigait.com>
2023-01-13 19:09:08 +0530

    admin and auth middleware added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+function auth(req, res, next) {
+  // console.log(req.headers.x-auth-token)
+  const token = req.header('bearer_token');
+  if(!token) return res.status(401).send('Access denied. No token provided.')
+
+  try{
+  const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
+  req.user = decoded;
+  next();
+  }
+  catch (er) {
+    res.status(400).send('Invalid Token.')
+  }
+}
+
+module.exports = auth;
