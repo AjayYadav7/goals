391120188b898d71e32c71613ca7d8ed84b82262
Ajayyadav18 <ajay.yadav@aurigait.com>
2023-01-18 14:48:30 +0530

    handled cors

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const registrations = require('./routes/registrations');
 const auth = require('./routes/auth');
+const cors = require('cors');
 const port = process.env.PORT || 4000;
 
 mongoose.set('strictQuery', true)
@@ -13,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/goalsDatabase')
 
 
 app.use(express.json())
-
+app.use(cors())
 app.get('/', (req,res) => {
   res.send('<h1>Home page</h1>')
 })
